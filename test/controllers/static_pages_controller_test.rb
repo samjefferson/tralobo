6062d4dfd55d54959e9645fd4b6b10ce47ef484b
Sam <sam_jefferson@hotmail.co.uk>
2015-07-30 23:18:56 +0100

    full title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Tralobo"
+    assert_select "title", "Tralobo"
   end
 
   test "should get help" do
