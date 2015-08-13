ce4ae46973ff45ce31c793988194eed34e05b4f1
Sam <sam_jefferson@hotmail.co.uk>
2015-08-13 15:00:02 +0100

    finished addin signup links

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,5 +6,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
  		assert_template 'static_pages/home'
  		assert_select "a[href=?]", root_path, count: 2
  		assert_select "a[href=?]", help_url
+ 		assert_select "a[href=?]", signup_path
 	end
 end
