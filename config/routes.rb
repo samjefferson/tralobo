1a7ee0c66f45fbd63f852a5e6628e3477faf8e7a
Sam <sam_jefferson@hotmail.co.uk>
2015-08-13 14:38:19 +0100

    some work on layout and new user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
   get 'help' => "static_pages#help"
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
