5d6261a9bd35ce99b3f405591705418d7850ecb1
Sam <sam_jefferson@hotmail.co.uk>
2015-08-13 21:49:44 +0100

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+  	@user = User.find(params[:id])
   end
+
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.create(user_params)
+  	if @user.save
+  		flash[:success] = "Sign Up Successful"
+  		redirect_to root_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  	end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test 'invalid signup' do
+		get signup_path
+		assert_no_difference 'User.count' do
+			post users_path, user: { username: ' ',
+															email: 'sam@sam',
+															password: 'jik',
+															password_confirmation: 'lol'} 
+		end	
+		assert_template 'users/new'
+  end
+
+  test 'valid signup' do
+  	get signup_path 
+  	assert_difference 'User.count', 1 do
+  		post_via_redirect users_path, user: { username: 'samjefferson',
+  															email: 'sam@example.com',
+  															password: 'password3',
+  															password_confirmation: 'password3'}
+  		end
+  end
+
+end
