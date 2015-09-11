2114bc3c9d7173e17f9f1be5d8d2983037b20c03
Sam <sam_jefferson@hotmail.co.uk>
2015-09-11 21:42:53 +0100

    add comment controller tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	def setup
+		@comment = comments(:test)
+		@admin_user = users(:test)
+		@user = users(:sissoko)
+	end
+
+	test "redirect non logged-in comment creates" do 
+		assert_no_difference 'Comment.count' do
+			post :create, comment: { content: "hello", user_id: 3, log_id: 2}
+		end
+		assert_redirected_to login_path
+	end
+
+	test "redirect non logged-in comment destroys" do
+		assert_no_difference 'Comment.count' do
+			post :destroy, id: @comment
+		end
+
+		assert_redirected_to root_url
+	end
+
+	test "redirect non admin user comment destroys" do
+		log_in_as @user
+		assert_no_difference 'Comment.count' do
+			post :destroy, id: @comment
+		end
+
+		assert_redirected_to root_url
+	end
+
 end
