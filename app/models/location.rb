6d81abd12e7a5674ecd6800b9b69c3fa8274b1d2
Sam <sam_jefferson@hotmail.co.uk>
2015-11-26 18:02:31 +0000

    minus coordin

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,7 +3,7 @@ class Location < ActiveRecord::Base
 
 	CITY_REGEX = /\A[A-Za-z\s]+\z/
 	validates :city, presence: true, length: { maximum: 25 }, format: { with: CITY_REGEX }, uniqueness: { case_sensitive: false }
-	LAT_LONG_REGEX = /\A\d+.\d+,\d+.\d+\z/
+	LAT_LONG_REGEX = /\A-?\d+.\d+,-?\d+.\d+\z/
 	validates :coordinate, format: { with: LAT_LONG_REGEX }
 	validates :continent, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0,
 					 less_than: 8 }
