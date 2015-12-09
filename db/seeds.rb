2160a9ac28bef237bab8f0343583e0800b1830f2
Sam <sam_jefferson@hotmail.co.uk>
2015-12-09 11:43:27 +0000

    removed top logs bug

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-User.create!(username:  "test123",
-             email: "test123@example.com",
-             password:              "samuel1",
-             password_confirmation: "samuel1",
-             admin: true)
